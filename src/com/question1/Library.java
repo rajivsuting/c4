9b74a84420da7370c1a47d900d243df87fd8c7e8
rajivsuting <rajivsuting16@gmail.com>
2022-09-24 09:47:45 -0700

    check

---
+++ src/com/question1/Library.java
@@ -0,0 +1,24 @@
+package com.question1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Library {
+	
+	  public static void main(String[] args) {
+			
+			Set<Book> booksCollection = new HashSet<>();
+			
+			booksCollection.add(new Book(1,"Name1","Author1"));
+			booksCollection.add(new Book(2,"Name2","Author1"));
+			booksCollection.add(new Book(3,"Name1","Author1"));
+		    booksCollection.add(new Book(1,"Name4","Author2"));
+
+	       for(Book b: booksCollection) {
+	    	   System.out.println(b);
+	       }
+		  
+		  
+	    }
+
+}
